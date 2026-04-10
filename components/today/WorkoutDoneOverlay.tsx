372e0e4862673257d470dcd1bacc4facfeda150d
WattWozy <niklas2@gmx.es>
2026-04-10 15:16:32 +0200

    nex session and db readme file

--- components/today/WorkoutDoneOverlay.tsx
+++ components/today/WorkoutDoneOverlay.tsx
@@ -66,7 +66,7 @@ export function WorkoutDoneOverlay({ visible }: WorkoutDoneOverlayProps) {
           cursor: 'pointer',
         }}
       >
-        New Session
+        Next Session
       </button>
     </div>
   );
